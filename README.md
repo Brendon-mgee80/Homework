0823e8e49f3006db2c93700bc914095690b5a2c1
Brendon McGee <brendon.mcgee80@gmail.com>
2024-12-01 16:58:03 +0000

    Markdown HW and Week2 JS

--- README.md
+++ README.md
@@ -3,4 +3,4 @@
 ### I'm a retired Army veteran from Memphis, TN who is excited about making his mark in the Software Development world!
 - I want to learn the skills necessary to jump start my career in Web Dev.
 - I want to confidently land my first job as a software engineer.
-- Have a solid Capstone Project that I continue to build upon for it to be a benefit those in the world.
+- Have a solid Capstone Project that I continue to build upon for it to be a benefit those in the world.git status
---
+++ Week2/Week2HW.js
@@ -0,0 +1,16 @@
+const pizzaPlace = "Best Pizza Slice";
+const numberOfToppings = 12;
+
+console.log("Pizza Place: ${pizzaPlace}, Type: ${typeof pizzaPlace}");
+console.log(
+  `Number of Toppings: ${numberOfToppings}, Type: ${typeof numberOfToppings}`
+);
+
+console.log(
+  `Welcome to ${pizzaPlace}, where we offer ${numberOfToppings} delicious toppings for your pizza!`
+);
+
+if (numberOfToppings < 10) {
+  console.log("Quality, not quantity.");
+} else {
+  console.log("A whole lot of pizza.");
